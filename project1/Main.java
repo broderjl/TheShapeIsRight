8cc1fe02562f048bb38fee3add9eb13a96198325
Jack Broderick <broderjl@Clarkson.edu>
2016-11-01 00:39:39 -0400

    Added drop down menu for selecting the number of shapes

--- project1/Main.java
+++ project1/Main.java
@@ -1,12 +1,15 @@
 package project1;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.event.*;
@@ -25,12 +28,47 @@ public class Main extends Application {
 			
 			
 			// main screen (Jack)
+			
+			/*
+			 * Variables !!!!! Not sure where they are going to need to go
+			 * or what type (i.e. final? public?)
+			 */
+			
+			int numberOfShapes;
+			
 			Label label1 = new Label("Main Menu:");
 			Button play_button = new Button("Play Game");
 			play_button.setOnAction(e -> window.setScene(scene2));
 			
+			/*
+			 * Drop down menu for selecting an N
+			 * at least 3 and at most 9
+			 */
+			
+			// Label for selection
+			Label selectionLabel = new Label("Number of Shapes You Would Like To Play With:");
+
+			
+			ObservableList<String> numberOfShapesList =
+					FXCollections.observableArrayList(
+							"3", "4", "5", "6", "7", "8", "9");
+			
+			ComboBox<String> numberOfShapesBox = new ComboBox<String>(numberOfShapesList);			
+			VBox selectionBox = new VBox();
+			selectionBox.getChildren().addAll(selectionLabel, numberOfShapesBox);
+			
+			/*
+			 * Event Handling
+			 */
+			
+			// Handling for the number of shapes selection
+			
+			/*
+			 * Adding the nodes to the scene
+			 */
+			
 			VBox main_screen = new VBox(20);
-			main_screen.getChildren().addAll(label1, play_button);
+			main_screen.getChildren().addAll(label1, play_button, selectionBox);
 			scene1 = new Scene(main_screen, 800, 600);
 			
 			
